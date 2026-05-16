5cf2cdf6b1ec3927ffe3d3d3c16124067eaf5d2a
Kazutov8 <kazutov.aa@garzdrav.ru>
2026-05-16 15:43:11 +0700

    feat: Добавить сваггер

--- src/IssueTracker.API/Program.cs
+++ src/IssueTracker.API/Program.cs
@@ -4,6 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();
 builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
@@ -11,6 +12,8 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
